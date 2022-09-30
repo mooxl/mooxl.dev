e476da6a98c19f60f8f60b76a396cef3bf3382f8
Max Schmidt <max.schmidt@outlook.de>
2022-09-30 11:26:46 +0200

    add fresh badge
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,18 +1,28 @@
 import { Translation } from "../utils/types.ts";
 
 const Footer = (data: { translation: Translation["footer"] }) => (
-  <div class="col-span-2 lg:col-span-1 flex items-center mt-10 justify-between">
+  <div class="col-span-2 lg:col-span-1 flex gap-1 items-start mt-10 justify-between">
     <a href="/impressum">
       {data.translation.imprint}
     </a>
-    <a
-      class="relative w-4"
-      href="https://github.com/mooxl"
-      rel="noopener"
-      target="_blank"
-    >
-      <img class="w-full" src="vectors/github.svg" alt="Logo von GitHub" />
-    </a>
+    <div class="flex flex-col items-center gap-2">
+      <a
+        class="relative w-4"
+        href="https://github.com/mooxl"
+        rel="noopener"
+        target="_blank"
+      >
+        <img class="w-full" src="vectors/github.svg" alt="Logo von GitHub" />
+      </a>
+      <a href="https://fresh.deno.dev">
+        <img
+          width="197"
+          height="37"
+          src="https://fresh.deno.dev/fresh-badge-dark.svg"
+          alt="Made with Fresh"
+        />
+      </a>
+    </div>
     <a href="/datenschutz">
       {data.translation.privacy}
     </a>
