b4a706b9f4ac4b318bad69811c132f80a65f2531
Max Schmidt <max.schmidt@outlook.de>
2022-10-01 00:55:33 +0200

    add type to twind.config.ts
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

--- twind.config.ts
+++ twind.config.ts
@@ -1,3 +1,4 @@
+import { Options } from "$fresh/plugins/twind.ts";
 export default {
   theme: {
     fontSize: {
@@ -75,33 +76,4 @@ export default {
       },
     },
   },
-  /* preflight: {
-    "@font-face": [
-      {
-        fontFamily: "Plex",
-        src: 'url(/fonts/Plex-Regular.woff2) format("woff2")',
-      },
-      {
-        fontFamily: "Plex",
-        fontWeight: "bold",
-        src: 'url(/fonts/Plex-Bold.woff2) format("woff2")',
-      },
-    ],
-
-    html: apply`text-html bg-gray`,
-    body: {
-      body: apply`text-sm text-white font-plex leading-none tracking-wide`,
-      h1: apply`text-xl leading-none font-bold sm:text-lg`,
-      h2: apply`text-lg font-bold text-gray-light sm:text-md`,
-      h3:
-        apply`text-right uppercase text-md font-bold text-gray-light lg:mt-6 lg:mb-2 md:mt-4 lg:text-left`,
-      h4: apply`text-white font-bold text-md  mb-1.5 lg:mb-1 md:mb-0.8`,
-      h5: apply`text-gray-light leading-snug`,
-      ul: apply`list-disc list-inside`,
-      li: apply`text-gray-light leading-normal`,
-      a: apply`text-gray-light leading-normal`,
-      p: apply`text-gray-light leading-snug`,
-      label: apply`text-gray-light leading-snug`,
-    },
-  }, */
-};
+} as Pick<Options, "theme">;
