074ca07512bc02cb561ec6e11138e1541e01d29b
Max Schmidt <max.schmidt@outlook.de>
2023-03-26 16:59:49 +0200

    Remove unnecessary anchor tag from _app.tsx

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -13,7 +13,6 @@ export default function App({ Component }: AppProps) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <a class="hidden" rel="me" href="https://fosstodon.org/@mooxl"></a>
       <div class="max-w-xl my-5 mx-auto lg:mx-2 sm:mx-1 text-sm text-white font-plex leading-none tracking-wide md:my-3">
         <Component />
       </div>
