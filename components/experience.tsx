d0e5ee368b43397b50c1cbdf3a15291c17c79871
Max Schmidt <max.schmidt@outlook.de>
2023-12-02 01:31:19 +0100

    update experiences
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

--- components/experience.tsx
+++ components/experience.tsx
@@ -5,10 +5,14 @@ const Milestone = (data: Translation["experience"]["jobs"][0]) => (
     <div class="flex justify-between md:flex-col">
       <div class="md:order-2">
         <h4>{data.name}</h4>
-        <h5>{data.role}</h5>
-        <ul>
-          {data.tasks.map((item: string) => <li key={item}>{item}</li>)}
-        </ul>
+        {data.stations.map((station) => (
+          <div class="mb-2">
+            <h5>{station.role}</h5>
+            <ul>
+              {station.tasks.map((item: string) => <li key={item}>{item}</li>)}
+            </ul>
+          </div>
+        ))}
       </div>
       <p class="flex-shrink-0 md:order-1 md:mb-0.2">{data.date}</p>
     </div>
