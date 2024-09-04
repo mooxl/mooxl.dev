8489c3680a1aff5fddd4d8a06a32088222f2a1d1
Max Schmidt <max.schmidt@outlook.de>
2024-09-04 15:59:53 +0200

    textual changes
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

--- routes/index.tsx
+++ routes/index.tsx
@@ -18,8 +18,8 @@ const Index = (ctx: RouteContext) => {
 		<div class="grid grid-cols-desktop gap-x-5 lg:grid-cols-1  gap-y-10 lg:gap-y-0">
 			<Menu lang={lang} />
 			<Me translation={translation.me} />
-			<Education translation={translation.education} />
 			<Experience translation={translation.experience} />
+			<Education translation={translation.education} />
 			<Skills translation={translation.skills} />
 			<Projects translation={translation.projects} />
 			<Contact translation={translation.contact} />
